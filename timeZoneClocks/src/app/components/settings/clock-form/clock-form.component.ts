cc4a31bcbda3695dbf996e4e3d02b017905c268d
Zach Sanford <afavorhouse2113@gmail.com>
2025-07-09 20:56:58 -0400

    clockList constructor

--- timeZoneClocks/src/app/components/settings/clock-form/clock-form.component.ts
+++ timeZoneClocks/src/app/components/settings/clock-form/clock-form.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { SettingsComponent } from '../settings.component';
+import { Clock } from '../../../interfaces/clock';
+import { ClocksService } from '../../../services/clocks.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-clock-form',
-  imports: [],
+  standalone: true,
+  imports: [SettingsComponent,FormsModule],
   templateUrl: './clock-form.component.html',
   styleUrl: './clock-form.component.css'
 })
 export class ClockFormComponent {
+
+    public clockList : Clock [] ;
+
+constructor(private clockService : ClocksService) {
+  this.clockList = clockService.getClockInfo();
+}
+
+timezone: any;
+  setTimezone(){
+    this.clockList[0].timeZone = this.timezone;
+  }
 
 }
